28e0c4efad68fa7bf12681b9a16c1dbf70d8569d
maxxnino <idonthaveemail@dontknow.com>
2021-07-10 02:36:44 +0900

    postprocess example

--- src/main.zig
+++ src/main.zig
@@ -1,76 +1,128 @@
 const std = @import("std");
-const ray = @import("translate-c/raylib_all.zig");
-
-pub fn getRandomValue(comptime T: type, min: i32, max: i32) T {
-    const re = ray.GetRandomValue(min, max);
-    switch (@typeInfo(T)) {
-        .Int => return @intCast(T, re),
-        .Float => return @intToFloat(T, re),
-        else => unreachable,
-    }
+const ray = @import("translate-c/raylib.zig");
+const PostProcessShader = enum {
+    bloom,
+    blur,
+    cross_hatching,
+    cross_stitching,
+    dream_vision,
+    fisheye,
+    grayscale,
+    pixelizer,
+    posterization,
+    predator_view,
+    scanlines,
+    sobel,
+};
+pub fn loadShader(shader_type: PostProcessShader) ray.Shader {
+    return switch (shader_type) {
+        .bloom => return ray.LoadShader(null, "3rd/raylib/examples/shaders/resources/shaders/glsl330/bloom.fs"),
+        .blur => return ray.LoadShader(null, "3rd/raylib/examples/shaders/resources/shaders/glsl330/blur.fs"),
+        .cross_hatching => return ray.LoadShader(null, "3rd/raylib/examples/shaders/resources/shaders/glsl330/cross_hatching.fs"),
+        .cross_stitching => return ray.LoadShader(null, "3rd/raylib/examples/shaders/resources/shaders/glsl330/cross_stitching.fs"),
+        .dream_vision => return ray.LoadShader(null, "3rd/raylib/examples/shaders/resources/shaders/glsl330/dream_vision.fs"),
+        .fisheye => return ray.LoadShader(null, "3rd/raylib/examples/shaders/resources/shaders/glsl330/fisheye.fs"),
+        .grayscale => return ray.LoadShader(null, "3rd/raylib/examples/shaders/resources/shaders/glsl330/grayscale.fs"),
+        .pixelizer => return ray.LoadShader(null, "3rd/raylib/examples/shaders/resources/shaders/glsl330/pixelizer.fs"),
+        .posterization => return ray.LoadShader(null, "3rd/raylib/examples/shaders/resources/shaders/glsl330/posterization.fs"),
+        .predator_view => return ray.LoadShader(null, "3rd/raylib/examples/shaders/resources/shaders/glsl330/predator.fs"),
+        .scanlines => return ray.LoadShader(null, "3rd/raylib/examples/shaders/resources/shaders/glsl330/scanlines.fs"),
+        .sobel => return ray.LoadShader(null, "3rd/raylib/examples/shaders/resources/shaders/glsl330/sobel.fs"),
+    };
 }
+pub fn unloadShader() void {}
 pub fn main() anyerror!void {
     // Initialization
     //--------------------------------------------------------------------------------------
     const screenWidth = 800;
     const screenHeight = 450;
-
-    ray.InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera first person");
+    const total_shader = @intCast(std.meta.Tag(PostProcessShader), std.meta.fields(PostProcessShader).len);
+    //ray.SetConfigFlags(ray.FLAG_MSAA_4X_HINT); // Enable Multi Sampling Anti Aliasing 4x (if available)
+    ray.InitWindow(screenWidth, screenHeight, "raylib [shaders] example - postprocessing shader");
     defer ray.CloseWindow();
+
     // Define the camera to look into our 3d world
-    var camera = ray.Camera3D{ .position = ray.Vector3{ .x = 4, .y = 2, .z = 4 }, .target = ray.Vector3{ .x = 0, .y = 1.8, .z = 0 }, .up = ray.Vector3{ .x = 0, .y = 1, .z = 0 }, .fovy = 60, .projection = ray.CAMERA_PERSPECTIVE };
-    // Generates some random columns
-    const max_columns = 20;
-    var heights = [_]f32{0} ** max_columns;
-    var positions = [_]ray.Vector3{ray.Vector3{ .x = 0, .y = 0, .z = 0 }} ** max_columns;
-    var colors = [_]ray.Color{ray.Color{ .r = 0, .g = 0, .b = 0, .a = 0 }} ** max_columns;
-    {
-        var i: usize = 0;
-        while (i < max_columns) : (i += 1) {
-            heights[i] = getRandomValue(f32, 1, 12);
-            positions[i] = ray.Vector3{ .x = getRandomValue(f32, -15, 15), .y = heights[i] / 2, .z = getRandomValue(f32, -15, 15) };
-            colors[i] = ray.Color{ .r = getRandomValue(u8, 20, 255), .g = getRandomValue(u8, 10, 55), .b = 30, .a = 255 };
-        }
+    var camera = ray.Camera{ .position = ray.Vector3{ .x = 2, .y = 3, .z = 2 }, .target = ray.Vector3{ .x = 0, .y = 1, .z = 0 }, .up = ray.Vector3{ .x = 0, .y = 1, .z = 0 }, .fovy = 45, .projection = ray.CAMERA_PERSPECTIVE };
+
+    var model = ray.LoadModel("3rd/raylib/examples/shaders/resources/models/church.obj"); // Load OBJ model
+    defer ray.UnloadModel(model); // Unload model
+
+    var texture = ray.LoadTexture("3rd/raylib/examples/shaders/resources/models/church_diffuse.png"); // Load model texture
+    defer ray.UnloadTexture(texture); // Unload texture
+    // (diffuse map)
+
+    model.materials[0].maps[ray.MATERIAL_MAP_DIFFUSE].texture = texture; // Set model diffuse texture
+
+    var position = ray.Vector3{ .x = 0, .y = 0, .z = 0 }; // Set model position
+
+    // Load all postpro shaders
+    // NOTE 1: All postpro shader use the base vertex shader
+    // (DEFAULT_VERTEX_SHADER) NOTE 2: We load the correct shader depending on
+    // GLSL version
+
+    var shaders: [total_shader]ray.Shader = undefined;
+    for (shaders) |*sh, i| {
+        sh.* = loadShader(@intToEnum(PostProcessShader, @intCast(std.meta.Tag(PostProcessShader), i)));
     }
-    ray.SetCameraMode(camera, ray.CAMERA_FIRST_PERSON); // Set a first person camera mode
-    ray.SetTargetFPS(60);
+    //TODO: find a way to deffer clean shader
+    // comptime for (shaders) |sh| {
+    //     defer ray.UnloadShader(sh);
+    // };
+    var current_shader = PostProcessShader.bloom;
+    // Create a RenderTexture2D to be used for render to texture
+    var target = ray.LoadRenderTexture(screenWidth, screenHeight);
+    defer ray.UnloadRenderTexture(target); // Unload render texture
+
+    // Setup orbital camera
+    ray.SetCameraMode(camera, ray.CAMERA_ORBITAL); // Set an orbital camera mode
+
+    ray.SetTargetFPS(60); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
     // Main game loop
     while (!ray.WindowShouldClose()) {
         // Update
         //----------------------------------------------------------------------------------
-        // const deltaTime = ray.GetFrameTime();
-        ray.UpdateCamera(&camera);
+        ray.UpdateCamera(&camera); // Update camera
+
+        if (ray.IsKeyPressed(ray.KEY_RIGHT)) {
+            current_shader = @intToEnum(PostProcessShader, (@enumToInt(current_shader) +% 1) % total_shader);
+        } else if (ray.IsKeyPressed(ray.KEY_LEFT)) {
+            current_shader = @intToEnum(PostProcessShader, (@enumToInt(current_shader) -% 1) % total_shader);
+        }
+
+        //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
+        ray.BeginTextureMode(target); // Enable drawing to texture
+        ray.ClearBackground(ray.RAYWHITE); // Clear texture background
+
+        ray.BeginMode3D(camera); // Begin 3d mode drawing
+        ray.DrawModel(model, position, 0.1, ray.WHITE); // Draw 3d model with texture
+        ray.DrawGrid(10, 1); // Draw a grid
+        ray.EndMode3D(); // End 3d mode drawing, returns to orthographic 2d mode
+        ray.EndTextureMode(); // End drawing to texture (now we have a texture available
+        // for next passes)
+
         ray.BeginDrawing();
-        ray.ClearBackground(ray.RAYWHITE);
+        ray.ClearBackground(ray.RAYWHITE); // Clear screen background
 
-        ray.BeginMode3D(camera);
+        // Render generated texture using selected postprocessing shader
+        ray.BeginShaderMode(shaders[@enumToInt(current_shader)]);
+        // NOTE: Render texture must be y-flipped due to default OpenGL coordinates
+        // (left-bottom)
+        ray.DrawTextureRec(target.texture, ray.Rectangle{ .x = 0, .y = 0, .width = @intToFloat(f32, target.texture.width), .height = @intToFloat(f32, -target.texture.height) }, ray.Vector2{ .x = 0, .y = 0 }, ray.WHITE);
+        ray.EndShaderMode();
 
-        ray.DrawPlane(ray.Vector3{ .x = 0, .y = 0, .z = 0 }, ray.Vector2{ .x = 32, .y = 32 }, ray.LIGHTGRAY); // Draw ground
-        ray.DrawCube(ray.Vector3{ .x = -16, .y = 2, .z = 0 }, 1, 5, 32, ray.BLUE); // Draw a blue wall
-        ray.DrawCube(ray.Vector3{ .x = 16, .y = 2, .z = 0 }, 1, 5, 32, ray.LIME); // Draw a green wall
-        ray.DrawCube(ray.Vector3{ .x = 0, .y = 2, .z = 16 }, 32, 5, 1, ray.GOLD); // Draw a yellow wall
+        // Draw 2d shapes and text over drawn texture
+        ray.DrawRectangle(0, 9, 580, 30, ray.Fade(ray.LIGHTGRAY, 0.7));
 
-        // Draw some cubes around
-
-        var i: usize = 0;
-        while (i < max_columns) : (i += 1) {
-            ray.DrawCube(positions[i], 2, heights[i], 2, colors[i]);
-            ray.DrawCubeWires(positions[i], 2, heights[i], 2, ray.MAROON);
-        }
-
-        ray.EndMode3D();
-
-        ray.DrawRectangle(10, 10, 220, 70, ray.Fade(ray.SKYBLUE, 0));
-        ray.DrawRectangleLines(10, 10, 220, 70, ray.BLUE);
-
-        ray.DrawText("First person camera default controls:", 20, 20, 10, ray.BLACK);
-        ray.DrawText("- Move with keys: W, A, S, D", 40, 40, 10, ray.DARKGRAY);
-        ray.DrawText("- Mouse move to look around", 40, 60, 10, ray.DARKGRAY);
+        ray.DrawText("(c) Church 3D model by Alberto Cano", screenWidth - 200, screenHeight - 20, 10, ray.GRAY);
+        ray.DrawText("CURRENT POSTPRO SHADER:", 10, 15, 20, ray.BLACK);
+        ray.DrawText(std.meta.tagName(current_shader).ptr, 330, 15, 20, ray.RED);
+        ray.DrawText("< >", 540, 10, 30, ray.DARKBLUE);
+        ray.DrawFPS(700, 15);
         ray.EndDrawing();
     }
 }
